596319dda4d55f278eea3e95410d328b52272927
HypeBenny <kwakudarko21@gmail.com>
2022-07-05 16:33:25 +0300

    1st code

---
+++ 1stcode.py
@@ -0,0 +1,38 @@
+#whem u wanna print hello n world on 2 diff lines
+print('hello \nworld')
+
+#input = aalows user to type in something
+name = input('please tell us your name?') #input function always return as strings even when a number isput
+print (name)
+
+#assigning strings to variables
+first_name = 'chris'
+last_name = 'waga'
+
+#use plus sign to combine 2 or more things/variables u wanna print
+print ('hello ' + first_name + ' ' + last_name)
+
+sentence = 'my son is kobby'
+
+#make sentence upper case, 
+print (sentence.upper())
+
+#makes sentence lower case
+print(sentence.lower())
+
+#count letter o and cap 1st letter.
+print (sentence.count('o'))
+
+#capitalize the first letter of the sentence
+print (sentence.capitalize())
+
+print ('hello ' + first_name.capitalize() + ' '  + last_name.upper())
+
+
+
+
+
+
+
+
+
